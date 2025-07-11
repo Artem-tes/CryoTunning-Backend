0665b8fcffe6b880a7823e14f20640fe814f72d3
artempotapov <strim20092509potapov@yandex.ru>
2025-07-11 16:23:43 +0300

    DeleteCarService is created but not tested

---
+++ src/test/java/com/cryotunning/cryotunning/CreateCarServiceTest.java
@@ -0,0 +1,138 @@
+package com.cryotunning.cryotunning;
+
+import com.cryotunning.cryotunning.customexception.NullFindCarInfoException;
+import com.cryotunning.cryotunning.customexception.UserCannotCreateCarException;
+import com.cryotunning.cryotunning.entities.User;
+import com.cryotunning.cryotunning.entities.dbentities.*;
+import com.cryotunning.cryotunning.entities.requestdto.CarResponseDTO;
+import com.cryotunning.cryotunning.entities.requestdto.CreateCarDTO;
+import com.cryotunning.cryotunning.repository.UserRepository;
+import com.cryotunning.cryotunning.repository.carpackage.*;
+import com.cryotunning.cryotunning.service.servicesclass.CreateCarService;
+import com.cryotunning.cryotunning.serviceimpl.VehicleServiceIMPL;
+import org.junit.jupiter.api.NamedExecutable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.LinkedList;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateCarServiceTest {
+    @Mock
+    private CarRepository carRepository;
+    @Mock
+    private ColorRepository colorRepository;
+    @Mock
+    private BrandRepository brandRepository;
+    @Mock
+    private ModelRepository modelRepository;
+    @Mock
+    private GenerationRepository generationRepository;
+
+
+    @InjectMocks
+    CreateCarService createCarService;
+
+
+    // simple data test
+    @Test
+    public void testCreateCarEndPoint() {
+        //arrange valid method one
+        CreateCarDTO mockCarDTO = new CreateCarDTO();
+        mockCarDTO.setBrand("1");
+        mockCarDTO.setModel("2");
+        mockCarDTO.setGeneration("3");
+        mockCarDTO.setColor("4");
+        when(brandRepository.getIdByBrandNameOptional("1")).thenReturn(Optional.of(1));
+        when(modelRepository.getIdByModelNameOptional("2")).thenReturn(Optional.of(2));
+        when(generationRepository.getIdByGenerationNameOptional("3")).thenReturn(Optional.of(3));
+        when(colorRepository.getIdByNameColorOptional("4")).thenReturn(Optional.of(4));
+        //arrange valid method two
+        User mockUser = new User();
+        mockUser.setId(1);
+        LinkedList<CarEntity> listOfCar = new LinkedList<>();
+        for (int i = 0; i < 1; i++) {
+            listOfCar.add(new CarEntity());
+        }
+        when(carRepository.getAllCarById(1)).thenReturn(Optional.of(
+                listOfCar
+        ));
+        //act
+        ResponseEntity<CarResponseDTO> response = createCarService.execute(mockCarDTO,mockUser );
+        //assert
+        assertEquals(201, response.getStatusCodeValue());
+
+    }
+
+    // throw exception test
+    // 1 - throw NullFieldCarInfoException(cannot resolve связи между id - name)
+    @Test
+    public void testNullFieldCarException(){
+        // assert
+        CreateCarDTO carDTO = new CreateCarDTO();
+        carDTO.setColor("1");
+        carDTO.setGeneration("1");
+        carDTO.setModel("1");
+        carDTO.setBrand("1");
+        when(brandRepository.getIdByBrandNameOptional(carDTO.getBrand()))
+                .thenReturn(Optional.empty());
+        when(modelRepository.getIdByModelNameOptional(carDTO.getModel()))
+                .thenReturn(Optional.empty());
+        when(generationRepository.getIdByGenerationNameOptional(carDTO.getGeneration()))
+                .thenReturn(Optional.empty());
+        when(colorRepository.getIdByNameColorOptional(carDTO.getColor()))
+                .thenReturn(Optional.empty());
+        //check
+        assertThrows(NullFindCarInfoException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                createCarService.execute(carDTO,new User());
+            }
+        });
+    }
+
+    //2 - UserCannotCreateCarException
+    @Test
+    public void testUserCannotCreateCarException(){
+        CreateCarDTO carDTO = new CreateCarDTO();
+        carDTO.setColor("1");
+        carDTO.setGeneration("1");
+        carDTO.setModel("1");
+        carDTO.setBrand("1");
+        User mockUser = new User();
+        mockUser.setId(1);
+        LinkedList<CarEntity> carEntities = new LinkedList<>();
+        for (int i = 0; i < 7; i++) {
+            carEntities.add(new CarEntity());
+        }
+        when(brandRepository.getIdByBrandNameOptional(carDTO.getBrand()))
+                .thenReturn(Optional.of(1));
+        when(modelRepository.getIdByModelNameOptional(carDTO.getModel()))
+                .thenReturn(Optional.of(1));
+        when(generationRepository.getIdByGenerationNameOptional(carDTO.getGeneration()))
+                .thenReturn(Optional.of(1));
+        when(colorRepository.getIdByNameColorOptional(carDTO.getColor()))
+                .thenReturn(Optional.of(1));
+        when(carRepository.getAllCarById(1)).thenReturn(
+             Optional.of(carEntities)
+        );
+        //check
+        assertThrows(UserCannotCreateCarException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                createCarService.execute(carDTO,mockUser);
+            }
+        });
+    }
+
+}
