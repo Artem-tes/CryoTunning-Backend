d9e34aa8d09c62f2bea051071b28d14a80a956cf
artempotapov <strim20092509potapov@yandex.ru>
2025-07-23 16:53:13 +0300

    Закончен LoginService - Документация Swagger,JavaDoc. Тестирование

---
+++ src/main/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.cryotunning.cryotunning.service.servicesclass"}];updateSearchResults();
---
+++ src/main/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.cryotunning.cryotunning.service.servicesclass","l":"LoginService"}];updateSearchResults();
